65c22a34f5536f8d05efeee484ea030605bf939f
ELOI <=eloi.pages@estudiantat.upc.edu>
2025-12-08 18:11:43 +0100

    ns què he fet erò crec que configurar coses

---
+++ create_user_airflow.sh
@@ -0,0 +1,6 @@
+airflow users create \
+    --username eloi_admin \
+    --firstname Eloi \
+    --lastname Pages \
+    --role Admin \
+    --email eloipages@gmail.com
--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,3 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export AIRFLOW_HOME="$SCRIPT_DIR/airflow"
-source "$SCRIPT_DIR/../../venv-BDA/bin/activate"
 echo "Airflow Environment Loaded!"
